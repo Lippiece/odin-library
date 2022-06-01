ceac6685089c25d45106bd6b85be03688c9f45a0
Lippiece <seriouss7222@gmail.com>
2022-06-01 16:06:47 +0200

    feat: add a function to generate form content
    it is not yet shown

--- scripts.js
+++ scripts.js
@@ -85,4 +85,59 @@ show.addEventListener("click", () => {
 });
 
 /* An array that will hold the elements of the add form. */
-const formAddSectionElements = [];
+const addFormSection = [];
+/**
+ * It creates a form section for adding books to the bookshelves.
+ */
+function populateAddFormSection() {
+	tip.setAttribute("style", "opacity:0.5");
+	formSection.append(tip);
+	
+	// Title
+	const nameLabel             = document.createElement("label");
+	nameLabel.textContent = "Book title:";
+	const nameInput             = document.createElement("input");
+	nameInput.name        = "name";
+	nameInput.placeholder = "Sherlock Holmes";
+	nameInput.required    = true;
+	nameLabel.append(nameInput);
+	addFormSection.push(nameLabel);
+
+	// Author
+	const authorLabel             = document.createElement("label");
+	authorLabel.textContent = "Author:";
+	const authorInput             = document.createElement("input");
+	authorInput.name        = "author";
+	authorInput.placeholder = "Dostoevsky";
+	authorInput.required    = true;
+	authorLabel.append(authorInput);
+	addFormSection.push(authorLabel);
+
+	// Length
+	const lengthLabel             = document.createElement("label");
+	lengthLabel.textContent = "Pages:";
+	const lengthInput             = document.createElement("input");
+	lengthInput.name        = "length";
+	lengthInput.placeholder = "100";
+	lengthInput.required    = true;
+	lengthLabel.append(lengthInput);
+	addFormSection.push(lengthLabel);
+
+	// Genre
+	const genreLabel             = document.createElement("label");
+	genreLabel.textContent = "Genre:";
+	const genreInput             = document.createElement("input");
+	genreInput.name        = "genre";
+	genreInput.placeholder = "Literary fiction";
+	genreInput.required    = true;
+	genreLabel.append(genreInput);
+	addFormSection.push(genreLabel);
+
+	// Submit
+	const submitButton             = document.createElement("button");
+	submitButton.textContent = "Add";
+	// submitButton.type        = "reset";
+	addFormSection.push(submitButton);
+
+	});
+}
