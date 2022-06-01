ca95a2729bca5b09df428da13bbdd7a370679a2c
Lippiece <seriouss7222@gmail.com>
2022-06-01 16:16:22 +0200

    refactor: format and add misc code changes

--- scripts.js
+++ scripts.js
@@ -1,37 +1,40 @@
-const buttons = document.querySelectorAll("button");
-// const add = buttons[0];
-const show = buttons[1];
-const remove = buttons[2];
-
-const sections = document.querySelectorAll("section");
-// const controlsSection = sections[0];
-const booksSection = sections[1];
-
-const divs = document.querySelectorAll("div");
-const shelves = divs[0];
-
-const books = [];
+/* buttons */
+const buttons = document.querySelectorAll("button"),
+	    add     = buttons[0],
+	    show    = buttons[1],
+	/* 	
+	sections */
+	sections     = document.querySelectorAll("section"),
+	booksSection = sections[1],
+	formSection  = sections[2],
+	/* 
+	divs */
+	divs    = document.querySelectorAll("div"),
+	shelves = divs[0],
+	/* 
+	main books array */
+	books = [];
 
 /**
- * The function Book() is a constructor function that creates a new object with the properties name,
- * author, length, and genre
- * @param name - the name of the book
- * @param author - "J.K. Rowling"
- * @param length - number of pages
- * @param genre - "Fiction"
- * @returns the string of the book's name, author, length, and genre.
- */
+	* The function Book() is a constructor function that creates a new object with the properties name,
+	* author, length, and genre
+	* @param name - the name of the book
+	* @param author - "J.K. Rowling"
+	* @param length - number of pages
+	* @param genre - "Fiction"
+	* @returns the string of the book's name, author, length, and genre.
+	*/
 function Book(name, author, length, genre) {
-	this.name = name;
+	this.name   = name;
 	this.author = author;
 	this.length = length;
-	this.genre = genre;
+	this.genre  = genre;
 
 	this.desc = function () {
 		return `Name: "${this.name}"<br>
 		Author: ${this.author}<br>
-		Length: ${this.length}<br> 
-		Genre: ${this.genre}<br>`;
+		Length: ${this.length}<br>
+		Genre : ${this.genre}<br>`;
 	};
 
 	/* Pushing the new object into the array of books. */
@@ -42,19 +45,19 @@ const _sample1 = new Book("Ulysses", "James Joyce", 661, "Modernist novel"),
 	bookshelves = [];
 /**
 	* Populate the books section with shelves.
- */
+	*/
 function populateShelves() {
 	const shelf    = document.createElement("article"),
 		desc         = document.createElement("p"),
 		removeButton = document.createElement("button");
 
-		shelves.append(shelf);
-		shelf.className = "shelf";
+	shelves.append(shelf);
+	shelf.className = "shelf";
 
 	shelf.append(desc, removeButton);
-		desc.className = "desc";
-		bookshelves.push(shelf);
-	}
+	desc.className = "desc";
+	bookshelves.push(shelf);
+}
 const tip = document.createElement("p");
 /* A boolean to avoid duplication. */
 let booksShown = false;
@@ -65,19 +68,13 @@ show.addEventListener("click", () => {
 		tip.textContent = "";
 		for (const _number of books) {
 			populateShelves();
-}
-const intro = document.createElement("h2");
-/* When the show button is clicked, it will add the text
-"We currently have:" to the intro. It will then prepend the intro to the
-booksSection. It will then loop through the books array and add the 
-book description to the bookshelves array. */
-show.addEventListener("click", () => {
-	populateBooksSection();
-	intro.textContent = "We currently have:";
-	booksSection.prepend(intro);
+		}
+		const intro = document.createElement("h2");
+		intro.textContent = "We currently have:";
+		booksSection.prepend(intro);
 
-	for (const [index, book] of books.entries()) {
-		bookshelves[index].children[0].innerHTML = book.desc();
+		for (const [index, book] of books.entries()) {
+			bookshelves[index].children[0].innerHTML = book.desc();
 		}
 
 		booksShown = true;
